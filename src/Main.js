de2eff38db59f20d9430f666caf122dbe842bdac
jamescrowell <jimmycrowell06@comcast.net>
2024-04-10 09:42:03 -0400

    -Add file validation, remove redudant code

--- src/Main.js
+++ src/Main.js
@@ -1,37 +1,34 @@
 import './App.css';
 import { useState } from 'react';
 import axios from 'axios';
-import { TextField } from '@mui/material';
 import Header from './Header.js';
 import Instructions from './Instructions.js';
 import ImageBanner from './ImageBanner.js'
-import { upload } from '@testing-library/user-event/dist/upload.js';
-
 
 function App() {
-  const [inputText, setInputText] = useState("asdasd");
-  const [displayText, setDisplayText] = useState("");
   const [uploadFile, setUploadFile] = useState(null);
+  const [footerNote, setFooterNote] = useState("No file uploaded.");
 
   const onFileUpload = e => {
       const file = e.target.files;
+      
       if (file && file.length > 0) {
         setUploadFile(file[0]);
+        setFooterNote("Uploaded file" + file[0].name);
       }
   }
 
-  const getBackend = () => {
-    axios.get(`http://localhost:8080/testBackend/${inputText}`)
-    .then(res => {
-      setDisplayText(res.data.message);
-    })
-    .catch(err => {
-      console.log(err);
-      setInputText("Could not retrieve data: " + err);
-    });
-  }
+  const sendFile = e => {
+    e.preventDefault();
+    const ext = uploadFile.name.split('.').pop();
+        
+    if (ext !== "csv") {
+      setFooterNote("Invalid file format, upload a .csv file");
+      return;
+    }
 
-  const sendFile = () => {
+    setFooterNote("Uploaded file " + uploadFile.name);
+
     let formData = new FormData();
     formData.append('file', uploadFile);
     console.log(formData);
@@ -56,16 +53,14 @@ function App() {
       <div className="fileUpload">
         <form>
           <h1>UPLOAD FILE</h1>
-          <input type="file" onChange={e => onFileUpload(e)}/>
-          <button type="submit">Upload</button>
+          <input type="file" accept=".csv" onChange={e => onFileUpload(e)}/>
+          <button onClick={e => sendFile(e)}>Upload</button>
+          <h3>{footerNote}</h3>
         </form>
       </div>
       
       <div className='footer'>
-        <h1>{uploadFile == null ? "No file uploaded." : "You uploaded: " + uploadFile.name}</h1>
-        <button onClick={sendFile}>Test File Upload</button>
       </div>
-
     </div>
   );
 }
